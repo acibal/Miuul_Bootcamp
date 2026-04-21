1eb4591282e43ca2f99df6f4a4fe32f610f85562
Atakan ACIBAL <138359341+acibal@users.noreply.github.com>
2026-04-21 21:43:13 +0300

    Add files via upload
    item based collaborative filtering

---
+++ Recommendation/item_based_recommender_acibal.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def create_user_movie_df():
+    movie = pd.read_csv('datasets/movie_lens_dataset/movie.csv')
+    rating = pd.read_csv('datasets/movie_lens_dataset/rating.csv')
+    df = movie.merge(rating, how="left", on="movieId")
+    comment_counts = pd.DataFrame(df["title"].value_counts())
+    rare_movies = comment_counts[comment_counts["count"] <= 1000].index
+    common_movies = df[~df["title"].isin(rare_movies)]
+    user_movie_df = common_movies.pivot_table(index=["userId"], columns=["title"], values="rating")
+    return user_movie_df
+
+user_movie_df = create_user_movie_df()
+
+def check_film(keyword, user_movie_df):
+    return [col for col in user_movie_df.columns if keyword in col]
+
+def item_based_recommender(movie_name, user_movie_df):
+    movie_name = user_movie_df[movie_name]
+    return user_movie_df.corrwith(movie_name).sort_values(ascending=False).head(10)
+
+movie_name = pd.Series(user_movie_df.columns).sample(1).values[0]
+
+item_based_recommender(movie_name, user_movie_df)
